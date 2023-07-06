4969eda26cc7c963e766882c3eb537ee5b4f4edf
yeffreydev <yeffreyespinoza200@gmail.com>
2023-07-06 13:12:51 -0500

    added config to server

--- projects/web/src/index.js
+++ projects/web/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 
+//config
+const config = require("./config");
+
 const app = express();
 
 app.use(express.json());
@@ -9,5 +12,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "./../public/")));
 app.use("/docs", express.static(path.join(__dirname, "./../docs/")));
 
-app.listen(4000, () => console.log("server running on port 4000"));
+app.listen(config.PORT, () => console.log(`server running on port ${config.PORT}`));
 module.exports = app;
